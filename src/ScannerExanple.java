b2f48a27597122e01809f44fc76b922607181fde
sourav6200 <sourav9931152709@gmail.com>
2024-01-08 11:47:25 +0530

    first program
    my first program

---
+++ src/ScannerExanple.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class ScannerExanple {
+    public static void main(String[] args) {
+        int roll= 55;
+        Scanner in =new Scanner (System.in);
+        System.out.println("Enter some input");
+        System.out.println( in.nextInt());
+        System.out.println(" sourav");
+    }
+}
